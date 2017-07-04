0841e16e3b78f7be46c72936c26f0c57593970b7
Tarun Rao <tarun.rao@bankofloyal.com>
2017-07-04 15:42:20 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
-import pygame,sys
+import pygame,sys,time
 import random as R
 from pygame.locals import *
+
+FPS = 25
+BLACK = pygame.Color( 0 , 0  , 0  )
+WHITE = pygame.Color(255, 255, 255)
+RED   = pygame.Color(255, 0  , 0  )
+GREEN = pygame.Color( 0 , 255, 0  )
+BLUE  = pygame.Color( 0 , 0  , 255)
+SCREENX=640
+SCREENY=480
+SPEED=10
 
 def collision(A,B):
     horizontal=False
@@ -14,20 +24,14 @@ def collision(A,B):
     elif (A.top>B.top) and (A.top<B.bottom):
         horizontal = True
     return horizontal and vertical
-
 def game_over():
-    print 'GAME OVER'
-    print 'score',snake.score
-
-FPS = 15
-BLACK = pygame.Color( 0 , 0  , 0  )
-WHITE = pygame.Color(255, 255, 255)
-RED   = pygame.Color(255, 0  , 0  )
-GREEN = pygame.Color( 0 , 255, 0  )
-BLUE  = pygame.Color( 0 , 0  , 255)
-SCREENX=640
-SCREENY=480
-SPEED=10
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render('GAME OVER', False, BLACK)
+    SCREEN.blit(textsurface,(0,0))
+    textsurface = myfont.render('SCORE : '+str(snake.score), False, BLACK)
+    SCREEN.blit(textsurface,(0,30))
+    time.sleep(5)
 
 class Snake:
     def __init__(self,head):
@@ -49,12 +53,12 @@ pygame.init()
 pygame.display.set_caption('Snake Game')
 SCREEN = pygame.display.set_mode((SCREENX,SCREENY))
 fpsClock=pygame.time.Clock()
-direction=''
 
-head = pygame.Rect(320,240,10,10)
+direction=''
+suicide=False
+head = pygame.Rect(SCREENX/2,SCREENY/2,10,10)
 snake=Snake(head)
 apple = pygame.Rect(R.randint(0,SCREENX-head.width),R.randint(0,SCREENY-head.height),20,20)
-
 while True:
     SCREEN.fill(WHITE)
     for event in pygame.event.get():
@@ -83,24 +87,20 @@ while True:
         head.x-=snake.speed
     elif(direction=='up'):
         head.y-=snake.speed
-    if(head.x==0 or head.x==SCREENX):
+    if(head.x==0 or head.x==SCREENX) and (direction!='up' and direction !='down'):
         head.x=SCREENX-head.x
-    if(head.y==0 or head.y==SCREENY):
+    if(head.y==0 or head.y==SCREENY) and (direction!='right' and direction !='left'):
         head.y=SCREENY-head.y
-
     if collision(apple,snake.body[0]):
          apple.left=R.randint(0,620)
          apple.top=R.randint(0,460)
          snake.add()
          snake.score+=1
-
-    suicide=False
     for i in snake.body[3:]:
         if collision(i,snake.body[0]):
             suicide=True
     if suicide:
         game_over()
-
     snake.draw(head)
     pygame.draw.rect(SCREEN,GREEN,(apple.left,apple.top,apple.width,apple.height))
     pygame.display.update()
